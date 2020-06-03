6c13875363c8b823d201eaa5936b9296491fcea9
jervygu <jervygu@gmail.com>
2020-06-03 13:20:01 +0800

    Built iPhone interface

--- dev-profile/dev-profile/ViewController.swift
+++ dev-profile/dev-profile/ViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        profileImage.layer.cornerRadius = 7
         // Do any additional setup after loading the view.
     }
 
